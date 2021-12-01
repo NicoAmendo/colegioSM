65c806809365be555d7368db59764cdfe53eed02
namendolara <namendolara@mininterior.gob.ar>
2021-12-01 20:37:20 -0300

    agrego log

--- suscriptores.php
+++ suscriptores.php
@@ -5,6 +5,9 @@ $nombre = $_POST['email'];
 
 echo $nombre;
 
+$json = json_encode($nombre);
+$bytes = file_put_contents("myfile.json", $json); 
+
 $log_message =  $nombre;
 $logFile = fopen("./log.txt", 'a');
 fwrite($logFile, "\n" . date("d/m/Y H:i:s") . " $log_message") or die("Error escribiendo en el archivo");
